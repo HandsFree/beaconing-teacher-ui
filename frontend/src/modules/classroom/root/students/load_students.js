ad29088293df222082c9cb8936a5b1b011455016
Felix Angell <felixangell@members.fsf.org>
2018-03-14 00:47:14 +0000

    some more dbeug logs to see whats going on...

--- frontend/src/modules/classroom/root/students/load_students.js
+++ frontend/src/modules/classroom/root/students/load_students.js
@@ -7,34 +7,13 @@ import StudentBox from './student_box';
 /* eslint-disable no-restricted-syntax */
 
 class LoadStudents extends Component {
-    async forceStudentLoad() {
-        console.log('[API Core] Loading students from beaconing API!');
-
-        let students = await window.beaconingAPI.getStudents();
-        if (!Array.isArray(students)) {
-            console.log('Failed to load students!');
-
-            // set the students in the state to an
-            // empty array to avoid error later on.
-            students = [];
-        }
-
-        this.state.students = students;
-        console.log(`Loaded students ${students}`);
-
-        window.sessionStorage.setItem('students', JSON.stringify({
-            students: this.state.students,
-            time: Date.now(),
-        }));
-    }
-
     async init() {
-        if (window.sessionStorage && window.sessionStorage.length > 0) {
+        if (window.sessionStorage) {
             console.log('Loading students from session storage');
             let studentsSession = window.sessionStorage.getItem('students');
 
             if (studentsSession) {
-                // console.log(sessionGLP);
+                console.log('Loading students from session');
                 studentsSession = JSON.parse(studentsSession);
 
                 if ((Date.now() / 60000) - (studentsSession.time / 60000) < 1) {
@@ -42,16 +21,30 @@ class LoadStudents extends Component {
                 }
             }
         } else {
-            this.forceStudentLoad();
+            console.log('[API Core] Loading students from beaconing API!');
+
+            const students = await window.beaconingAPI.getStudents();
+            if (!Array.isArray(students)) {
+                console.log('Failed to load students!');
+    
+                // set the students in the state to an
+                // empty array to avoid error later on.
+                this.state.students = [];
+            } else {
+                this.state.students = students;
+            }
+    
+            console.log(`Loaded students ${students}`);
+    
+            window.sessionStorage.setItem('students', JSON.stringify({
+                students: this.state.students,
+                time: Date.now(),
+            }));
         }
     }
 
     async render() {
-        if (!Array.isArray(this.state.students) || this.state.students.length === 0) {
-            this.forceStudentLoad();
-        }
-
-        const students = Object.values(this.state.students);
+        const students = Array.isArray(this.state.students) ? Object.values(this.state.students) : [];
         const promArr = [];
 
         for (const student of students) {
