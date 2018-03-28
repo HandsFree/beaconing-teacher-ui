1d5a50727531010677eef3a990cdd11850a1a1fc
juddus <e@juddus.com>
2018-03-28 13:06:47 +0100

    tweaked student type

--- backend/types/student.go
+++ backend/types/student.go
@@ -1,7 +1,9 @@
 package types
 
 type Student struct {
-	Id              uint64 `json:"id"`
-	Username        string `json:"username"`
-	IdenticonSha512 string `json:"identiconSha512"`
+	Id              uint64      `json:"id"`
+	Username        string      `json:"username"`
+	Email           string      `json:"email"`
+	Profile         interface{} `json:"profile"`
+	IdenticonSha512 string      `json:"identiconSha512"`
 }
