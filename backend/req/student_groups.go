9ebe0f0a1b3cf09e6feb0989c55bfa684252d95e
Felix Angell <felixangell@members.fsf.org>
2018-04-14 15:45:13 +0100

    filtering student groups by category, closes #125

--- backend/req/student_groups.go
+++ backend/req/student_groups.go
@@ -4,10 +4,33 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"git.juddus.com/HFC/beaconing/backend/api"
+	"git.juddus.com/HFC/beaconing/backend/types"
 	"github.com/gin-gonic/gin"
+	jsoniter "github.com/json-iterator/go"
 )
+
+func sortStudentGroups(groups []*types.StudentGroup, filterBy string, match string) []*types.StudentGroup {
+	// we are over allocating here but
+	// it doesnt hurt anyone
+
+	switch filterBy {
+	case "category":
+		result := []*types.StudentGroup{}
+		for _, grp := range groups {
+			if strings.Compare(grp.Category, match) == 0 {
+				result = append(result, grp)
+			}
+		}
+		return result
+	default:
+		log.Println("Attempted to sort student groups by", filterBy, "=>", match)
+		// NOP
+		return groups
+	}
+}
 
 func PostStudentGroupRequest() gin.HandlerFunc {
 	return func(s *gin.Context) {
@@ -48,6 +71,16 @@ func DeleteStudentGroupRequest() gin.HandlerFunc {
 
 func GetStudentGroupsRequest() gin.HandlerFunc {
 	return func(s *gin.Context) {
+		// what to filter the studentgroups by
+		// for example, "category"
+		filterBy := s.Query("filter")
+
+		// used with sortBy this is what
+		// the field should match _identially_
+		// for example, sortBy category and match class
+		// will make sure that value of category === "class"
+		match := s.Query("match")
+
 		body, err := api.GetStudentGroups(s)
 		if err != nil {
 			log.Println("GetStudentGroupsRequest", err.Error())
@@ -55,8 +88,30 @@ func GetStudentGroupsRequest() gin.HandlerFunc {
 			return
 		}
 
+		if filterBy == "" && match == "" {
+			s.Header("Content-Type", "application/json")
+			s.String(http.StatusOK, body)
+			return
+		}
+
+		var groups []*types.StudentGroup
+		err = jsoniter.Unmarshal([]byte(body), &groups)
+		if err != nil {
+			log.Println(err.Error())
+			s.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+
+		filteredGroups := sortStudentGroups(groups, filterBy, match)
+		data, err := jsoniter.Marshal(&filteredGroups)
+		if err != nil {
+			log.Println(err.Error())
+			s.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+
 		s.Header("Content-Type", "application/json")
-		s.String(http.StatusOK, body)
+		s.String(http.StatusOK, string(data))
 	}
 }
 
