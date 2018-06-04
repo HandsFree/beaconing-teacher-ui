37ac1134a72de17c2baaefa8cb151e1e05223045
Felix Angell <felixangell@members.fsf.org>
2018-06-04 15:59:31 +0100

    calendar event map now stores keys properly; days are offset correctly

--- frontend/src/modules/calendar/root/calendar/calendar.js
+++ frontend/src/modules/calendar/root/calendar/calendar.js
@@ -3,7 +3,60 @@
 import { section, h1, p, div, table, tr, th, td, ul, li } from '../../../../core/html';
 import { Component } from '../../../../core/component';
 
+Date.prototype.withoutTime = function () {
+    var d = new Date(this);
+    d.setHours(0, 0, 0, 0);
+    return d;
+}
+
 class CalendarView extends Component {
+	async init() {
+        this.state.eventMap = new Map();
+
+        // write some test events
+        this.writeEvent(new Date('June 5, 2018 03:24:00'),
+			{
+				name: "Foo",
+				desc: "Bar",	
+			});
+
+		this.writeEvent(new Date('June 12, 2018 05:32:00'),
+			{
+				name: "Foo2",
+				desc: "Bar2",	
+			});
+
+		this.writeEvent(new Date('June 19, 2018 06:12:00'),
+			{
+				name: "Foo3",
+				desc: "Bar3",	
+			});
+    }
+
+    // the event map stores key => value
+    // where the key is the date the event is set
+    // specifically the MM/DD/YYYY, 
+    // the value is an array of events. we append to this
+    // array or we insert an array when writing an event.
+    // note that we strip the time from the date given
+    // so that we can index the hashmap just from mm/dd/yyyy
+    writeEvent(eventDate, event) {
+    	// store the date in the event object
+    	// WITH the time included.
+    	event.date = eventDate;
+
+    	const newDate = eventDate.withoutTime().getTime();
+    	console.log("writing ", event, " as date ", eventDate, " is ", newDate);
+
+    	let events = this.state.eventMap.get(newDate);
+    	if (events) {
+    		events.push(event);
+    		// re-write into hashmap
+    		this.state.eventMap.set(newDate, events);
+    	} else {
+    		this.state.eventMap.set(newDate, [event]);
+    	}
+    }
 
 	// returns how many days there are in the
 	// month d
@@ -38,33 +91,32 @@ class CalendarView extends Component {
 	}
 
 	genCalendarData(date) {
-		// TODO load all of the event thingies
-		// key (date) => event list?
-		let eventMap = new Map();
+		const eventMap = this.state.eventMap;
+
+		const firstDay = new Date(date.getFullYear(), date.getMonth(), 0);
 
 		// all the dates this month
-		const cells = [];
-		for (var i = 0; i < this.daysInMonth(date); i++) {
-			const cellDate = new Date(date.getFullYear(), date.getMonth(), i);
-
-			if (i == 3) {
-				// testing!
-				eventMap.set(cellDate, [
-					{
-						name: "Example Event 1",
-						desc: "Example Event desc",
-					},
-				]);
-			}
-
-			cells[i] = cellDate;
-		}
 
 		let rows = [];
 
 		{
 			let rowBuffer = [];
-			for (var i = 0; i < cells.length; i++) {
+
+			const offset = firstDay.getDay();
+
+			// previous month
+			const prevMonth = new Date(firstDay - 1);
+			const prevMonthDays = this.daysInMonth(prevMonth);
+			const startDay = prevMonthDays - offset;
+
+			for (let i = 0; i < offset; i++) {
+				// TODO this should be greyed out
+				// events will not be fetched for previous month days
+				const dayNumber = (startDay + i)+1;
+				rowBuffer.push([p(dayNumber), []]);
+			}
+
+			for (var i = offset; i < offset + this.daysInMonth(date); i++) {
 
 				// every 7 days.
 				if (i % 7 == 0 && i > 0) {
@@ -72,19 +124,22 @@ class CalendarView extends Component {
 					rowBuffer = []; // reset
 				}
 
-				const cellDate = cells[i];
+				const cellDate = new Date(firstDay.getYear(), firstDay.getMonth()+1, i);
 				let eventList = div('.events');
 
-				if (eventMap.has(cellDate)) {
-					const evt = eventMap.get(cellDate);
+				if (eventMap.has(cellDate.getTime())) {
+					const evt = eventMap.get(cellDate.getTime());
 					eventList = div('.events', 
 						evt.map((evt, _) => div('.event',
 							// div('.event-name', p(evt.name)),
 							div('.event-desc', p(evt.desc)))
 					));
+				} else {
+					console.log(cellDate, " is not in evetn map");
 				}
 
-				rowBuffer.push([p(cellDate.getDay()), eventList]);
+				const dayNumber = (i - offset)+1;
+				rowBuffer.push([p(dayNumber), eventList]);
 			}
 		}
 
