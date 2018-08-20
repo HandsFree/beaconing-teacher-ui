445bdfc30d59e2b194db28d6d31bb996b752a650
Felix Angell <felixangell@members.fsf.org>
2018-08-20 17:18:31 +0100

    quick fix for #187

--- backend/util/log.go
+++ backend/util/log.go
@@ -39,11 +39,14 @@ func Info(msg ...interface{}) {
 
 func Log(level LogLevel, msg ...interface{}) {
 	when := fmt.Sprintf("%s: ", time.Now().Format("2006-01-02 15:04:05"))
+	pipe := os.Stdout
+
 	switch level {
 	case ErrorLog:
 		fallthrough
 	case FatalLog:
-		fmt.Print(Bold(Red(when)))
+		pipe = os.Stderr
+		fmt.Printf(Bold(Red(when)))
 	case VerboseLog:
 		fmt.Print(Bold(Green(when)))
 	case InfoLog:
@@ -51,17 +54,20 @@ func Log(level LogLevel, msg ...interface{}) {
 	case WarnLog:
 		fmt.Print(Bold(Yellow(when)))
 	}
-	fmt.Println(msg...)
+	fmt.Fprintln(pipe, msg...)
 }
 
 // BigLog is the same as Log, however the date
 // of the log is placed on its own line
 func BigLog(level LogLevel, msg ...interface{}) {
 	when := time.Now().Format("2006-01-02 15:04:05")
+	pipe := os.Stdout
+
 	switch level {
 	case ErrorLog:
 		fallthrough
 	case FatalLog:
+		pipe = os.Stderr
 		fmt.Println(Bold(Red(when)))
 	case VerboseLog:
 		fmt.Println(Bold(Green(when)))
@@ -70,5 +76,6 @@ func BigLog(level LogLevel, msg ...interface{}) {
 	case WarnLog:
 		fmt.Println(Bold(Yellow(when)))
 	}
-	fmt.Println(msg...)
+
+	fmt.Fprintln(pipe, msg...)
 }
