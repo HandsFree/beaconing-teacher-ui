f0b1adcf851239a544fdbce4d57e24aa2f4d3593
Elliott Judd <e@juddus.com>
2018-10-06 03:24:44 +0100

    updating old forms to current design (more info in pr)

--- frontend/src/modules/lesson_manager/edit_plan/edit_form.js
+++ frontend/src/modules/lesson_manager/edit_plan/edit_form.js
@@ -2,7 +2,6 @@
 import {
     section,
     div,
-    i,
     form,
     input,
     label,
@@ -12,11 +11,11 @@ import {
     textarea,
 } from '../../../core/html';
 
-import { Component } from '../../../core/component';
+import Form from '../../form';
 import Status from '../../status';
 import nullishCheck from '../../../core/util';
 
-class EditGLPForm extends Component {
+class EditGLPForm extends Form {
     stateObj = {
         planName: '',
         planCategory: 'science',
@@ -54,8 +53,6 @@ class EditGLPForm extends Component {
     };
 
     state = new Proxy(this.stateObj, this.stateProxy);
-
-    enabledErrors = [];
 
     glp = {};
 
@@ -100,107 +97,6 @@ class EditGLPForm extends Component {
     async resetSubmit() {
         const saveButton = document.getElementById('save-plan-button');
         saveButton.textContent = await window.bcnI18n.getPhrase('lm_save_plan');
-    }
-
-    removeErrors() {
-        for (const v of this.enabledErrors) {
-            this.removeError(v);
-        }
-    }
-
-    addError(elementID: string, errMsg: string) {
-        const el = document.getElementById(elementID);
-        const labelGroup = el?.parentElement?.parentElement;
-        const errEl = div(
-            '.flex-align-center',
-            i('.icon-cancel', { attrs: { 'aria-hidden': true } }),
-            span(errMsg),
-        );
-
-        if (this.enabledErrors.indexOf(elementID) === -1) {
-            this.enabledErrors.push(elementID);
-        }
-
-        if (nullishCheck(el, false) && nullishCheck(labelGroup, false)) {
-            labelGroup.classList.remove('loading');
-            labelGroup.classList.remove('success');
-            labelGroup.classList.add('error');
-            // console.log(el);
-            if (el.childElementCount > 0) {
-                el.replaceChild(errEl, el.firstElementChild);
-            }
-
-            el.appendChild(errEl);
-        }
-    }
-
-    addLoading(elementID: string) {
-        const el = document.getElementById(elementID);
-        const labelGroup = el?.parentElement?.parentElement;
-        const loadingEl = i('.icon-load', { attrs: { 'aria-hidden': true } });
-
-        if (nullishCheck(el, false) && nullishCheck(labelGroup, false)) {
-            labelGroup.classList.remove('error');
-            labelGroup.classList.remove('success');
-            labelGroup.classList.add('loading');
-            if (el.childElementCount > 0) {
-                el.replaceChild(loadingEl, el.firstElementChild);
-            }
-
-            el.appendChild(loadingEl);
-        }
-    }
-
-    addSuccess(elementID: string) {
-        const el = document.getElementById(elementID);
-        const labelGroup = el?.parentElement?.parentElement;
-        const successEl = i('.icon-ok', { attrs: { 'aria-hidden': true } });
-
-        if (nullishCheck(el, false) && nullishCheck(labelGroup, false)) {
-            if (this.enabledErrors.indexOf(elementID) !== -1) {
-                delete this.enabledErrors[elementID];
-                labelGroup.classList.remove('error');
-            }
-
-            labelGroup.classList.remove('loading');
-            labelGroup.classList.add('success');
-            if (el.childElementCount > 0) {
-                el.replaceChild(successEl, el.firstElementChild);
-            }
-
-            el.appendChild(successEl);
-        }
-    }
-
-    removeError(elementID: string) {
-        if (this.enabledErrors.indexOf(elementID) !== -1) {
-            const el = document.getElementById(elementID);
-            const labelGroup = el?.parentElement?.parentElement;
-
-            if (nullishCheck(el, false) && nullishCheck(labelGroup, false)) {
-                labelGroup.classList.remove('error');
-                el.innerHTML = '';
-            }
-
-            delete this.enabledErrors[elementID];
-        }
-    }
-
-    removeAll(elementID: string) {
-        const el = document.getElementById(elementID);
-        const labelGroup = el?.parentElement?.parentElement;
-
-        if (nullishCheck(el, false) && nullishCheck(labelGroup, false)) {
-            labelGroup.classList.remove('loading');
-            labelGroup.classList.remove('success');
-
-            if (this.enabledErrors.indexOf(elementID) !== -1) {
-                delete this.enabledErrors[elementID];
-                labelGroup.classList.remove('error');
-            }
-
-            el.innerHTML = '';
-        }
     }
 
     async checkGLPName() {
--- frontend/src/modules/lesson_manager/new_plan/new_plan_form.js
+++ frontend/src/modules/lesson_manager/new_plan/new_plan_form.js
@@ -14,12 +14,11 @@ import {
     option,
 } from '../../../core/html';
 
-import { Component } from '../../../core/component';
+import Form from '../../form';
 import Status from '../../status';
 import PostCreation from './post_creation';
-import nullishCheck from '../../../core/util';
 
-class NewPlanForm extends Component {
+class NewPlanForm extends Form {
     stateObj = {
         planName: '',
         planCategory: 'science',
@@ -62,8 +61,6 @@ class NewPlanForm extends Component {
         ResetForm: this.resetForm,
     };
 
-    enabledErrors = [];
-
     glps = {};
 
     async init() {
@@ -100,107 +97,6 @@ class NewPlanForm extends Component {
     async resetSubmit() {
         const planButton = document.getElementById('create-plan-button');
         planButton.textContent = await window.bcnI18n.getPhrase('lm_create_plan');
-    }
-
-    removeErrors() {
-        for (const v of this.enabledErrors) {
-            this.removeError(v);
-        }
-    }
-
-    addError(elementID: string, errMsg: string) {
-        const el = document.getElementById(elementID);
-        const labelGroup = el?.parentElement?.parentElement;
-        const errEl = div(
-            '.flex-align-center',
-            i('.icon-cancel', { attrs: { 'aria-hidden': true } }),
-            span(errMsg),
-        );
-
-        if (this.enabledErrors.indexOf(elementID) === -1) {
-            this.enabledErrors.push(elementID);
-        }
-
-        if (nullishCheck(el, false) && nullishCheck(labelGroup, false)) {
-            labelGroup.classList.remove('loading');
-            labelGroup.classList.remove('success');
-            labelGroup.classList.add('error');
-            // console.log(el);
-            if (el.childElementCount > 0) {
-                el.replaceChild(errEl, el.firstElementChild);
-            }
-
-            el.appendChild(errEl);
-        }
-    }
-
-    addLoading(elementID: string) {
-        const el = document.getElementById(elementID);
-        const labelGroup = el?.parentElement?.parentElement;
-        const loadingEl = i('.icon-load', { attrs: { 'aria-hidden': true } });
-
-        if (nullishCheck(el, false) && nullishCheck(labelGroup, false)) {
-            labelGroup.classList.remove('error');
-            labelGroup.classList.remove('success');
-            labelGroup.classList.add('loading');
-            if (el.childElementCount > 0) {
-                el.replaceChild(loadingEl, el.firstElementChild);
-            }
-
-            el.appendChild(loadingEl);
-        }
-    }
-
-    addSuccess(elementID: string) {
-        const el = document.getElementById(elementID);
-        const labelGroup = el?.parentElement?.parentElement;
-        const successEl = i('.icon-ok', { attrs: { 'aria-hidden': true } });
-
-        if (nullishCheck(el, false) && nullishCheck(labelGroup, false)) {
-            if (this.enabledErrors.indexOf(elementID) !== -1) {
-                delete this.enabledErrors[elementID];
-                labelGroup.classList.remove('error');
-            }
-
-            labelGroup.classList.remove('loading');
-            labelGroup.classList.add('success');
-            if (el.childElementCount > 0) {
-                el.replaceChild(successEl, el.firstElementChild);
-            }
-
-            el.appendChild(successEl);
-        }
-    }
-
-    removeError(elementID: string) {
-        if (this.enabledErrors.indexOf(elementID) !== -1) {
-            const el = document.getElementById(elementID);
-            const labelGroup = el?.parentElement?.parentElement;
-
-            if (nullishCheck(el, false) && nullishCheck(labelGroup, false)) {
-                labelGroup.classList.remove('error');
-                el.innerHTML = '';
-            }
-
-            delete this.enabledErrors[elementID];
-        }
-    }
-
-    removeAll(elementID: string) {
-        const el = document.getElementById(elementID);
-        const labelGroup = el?.parentElement?.parentElement;
-
-        if (nullishCheck(el, false) && nullishCheck(labelGroup, false)) {
-            labelGroup.classList.remove('loading');
-            labelGroup.classList.remove('success');
-
-            if (this.enabledErrors.indexOf(elementID) !== -1) {
-                delete this.enabledErrors[elementID];
-                labelGroup.classList.remove('error');
-            }
-
-            el.innerHTML = '';
-        }
     }
 
     async checkGLPName() {
