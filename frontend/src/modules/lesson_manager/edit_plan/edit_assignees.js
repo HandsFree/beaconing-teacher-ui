deb22cba718cbb591841fd1cef234052395ffb0d
Elliott Judd <e@juddus.com>
2019-03-09 07:50:11 +0000

    Add edit assignee page

---
+++ frontend/src/modules/lesson_manager/edit_plan/edit_assignees.js
@@ -0,0 +1,14 @@
+// @flow
+import {
+    div,
+} from '../../../core/html';
+import { Component } from '../../../core/component';
+
+class EditAssignees extends Component {
+    async render() {
+        return div();
+    }
+}
+
+
+export default EditAssignees;
